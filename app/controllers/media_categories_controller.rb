d78b4d7d8c617d63d3f05271fa136929653aa851
Cody Bouscaren <codybousc@gmail.com>
2015-07-01 15:26:46 -0700

    Allows for new category creation

--- app/controllers/media_categories_controller.rb
+++ app/controllers/media_categories_controller.rb
@@ -3,4 +3,27 @@ class MediaCategoriesController < ApplicationController
     @media_categories = MediaCategory.all
     render :index
   end
+
+  def show
+    @media_category = MediaCategory.find(params[:id])
+  end
+
+  def new
+    @media_category = MediaCategory.new
+  end
+
+  def create
+    @media_category = MediaCategory.new(media_category_params)
+    if @media_category.save
+      redirect_to media_categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def media_category_params
+    params.require(:media_category).permit(:category, :image_url)
+  end
+
 end
